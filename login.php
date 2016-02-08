213b96b35e27bc450ac32e3f0de14c040af98ddc
l33tkr3w <blackhatworks@gmail.com>
2016-02-08 16:42:40 -0700

    derp

--- login.php
+++ login.php
@@ -1,8 +1,4 @@
 <?php
-
-
-print_r($_SESSION['LoginStatus']);
-
 if (isset($_POST['email'])) {
     echo "Login true <br><br>";
 
@@ -34,42 +30,3 @@ if (isset($_POST['email'])) {
 
 }
 ?>
-
-
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-
-        <form method="POST" action="index.php">
-            <table border="1">
-                <thead>
-                    <tr>
-                        <th colspan="2">Login</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>username</td>
-                        <td><input type="text" name="email" value="" size="25" /></td>
-                    </tr>
-                    <tr>
-                        <td>password</td>
-                        <td><input type="text" name="password" value="" size="25" /></td>
-                    </tr>
-                    <tr>
-                        <td></td>
-                        <td><input type="submit" value="Enter" name="Enter" /></td>
-                    </tr>
-                </tbody>
-            </table>
-        </form>
-		<p><a href="index.php">Home Page</a></p>
-<?php
-// put your code here
-?>
-    </body>
-</html>
-
